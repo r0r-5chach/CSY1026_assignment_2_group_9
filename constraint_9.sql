817468fe136216280c4ee00d2a2ea091dd2995e8
aboolathings <hannah_james@outlook.com>
2022-05-22 21:24:14 +0100

    moved brackets due to addition on wrong line on constraints

--- constraint_9.sql
+++ constraint_9.sql
@@ -43,52 +43,52 @@ MODIFY (game_assists NOT NULL);
 -- alter primary keys
 
 ALTER TABLE players
-ADD CONSTRAINT (pk_players)
-PRIMARY KEY player_id;
+ADD CONSTRAINT pk_players
+PRIMARY KEY (player_id);
 
 ALTER TABLE teams
-ADD CONSTRAINT (pk_teams)
-PRIMARY KEY team_id;
+ADD CONSTRAINT pk_teams
+PRIMARY KEY (team_id);
 
 ALTER TABLE team_players
-ADD CONSTRAINT (pk_team_players)
-PRIMARY KEY team_player_id;
+ADD CONSTRAINT pk_team_players
+PRIMARY KEY (team_player_id);
 
 ALTER TABLE categories
-ADD CONSTRAINT (pk_categories)
-PRIMARY KEY genre_id;
+ADD CONSTRAINT pk_categories
+PRIMARY KEY (genre_id);
 
 ALTER TABLE tournaments
-ADD CONSTRAINT (pk_tournaments)
-PRIMARY KEY tournament_id;
+ADD CONSTRAINT pk_tournaments
+PRIMARY KEY (tournament_id);
 
 ALTER TABLE tournament_participants
-ADD CONSTRAINT (pk_tournament_participants)
-PRIMARY KEY tournament_participant_id
+ADD CONSTRAINT pk_tournament_participants
+PRIMARY KEY (tournament_participant_id);
 
 ALTER TABLE sponsors
-ADD CONSTRAINT (pk_sponsors)
-PRIMARY KEY sponsor_id;
+ADD CONSTRAINT pk_sponsors
+PRIMARY KEY (sponsor_id);
 
 ALTER TABLE prizes
-ADD CONSTRAINT (pk_prizes)
-PRIMARY KEY prize_id;
+ADD CONSTRAINT pk_prizes
+PRIMARY KEY (prize_id);
 
 ALTER TABLE rounds
-ADD CONSTRAINT (pk_rounds)
-PRIMARY KEY round_id;
+ADD CONSTRAINT pk_rounds
+PRIMARY KEY (round_id);
 
 ALTER TABLE games
-ADD CONSTRAINT (pk_games)
-PRIMARY KEY game_id;
+ADD CONSTRAINT pk_games
+PRIMARY KEY (game_id);
 
 ALTER TABLE statistics
-ADD CONSTRAINT (pk_statistics)
-PRIMARY KEY game_result_id;
+ADD CONSTRAINT pk_statistics
+PRIMARY KEY (game_result_id);
 
 ALTER TABLE game_participants
-ADD CONSTRAINT (pk_game_participants)
-PRIMARY KEY game_participant_id;
+ADD CONSTRAINT pk_game_participants
+PRIMARY KEY (game_participant_id);
 
 -- alter unique
 
