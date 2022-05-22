51fbf4a45bcead06d51c071c7d0aa9cb71584aa7
Joshua Perry <45966243+jpez-development@users.noreply.github.com>
2022-05-22 19:21:27 +0100

    fixes

--- queries_9.sql
+++ queries_9.sql
@@ -1,29 +1,21 @@
-ALTER SESSION;
-SET NLS_DATE_FORMAT = "DD-MON-YYYY HH24:MI:SS";
-SET PAGESIZE ??;
-SET LINESIZE ???;
-SET WRAP OFF;
+SELECT * FROM players WHERE player_id > 2;
 
-SELECT * FROM table_name WHERE table_name="&table_name";
-
-SELECT column_one, column_two, column_three FROM table_name 
-WHERE table_name="&table_name"
-AND column_one="&column_one"
-AND column_two="&column_two"
-AND column_three="&column_three"
-ORDER BY column_one DESC;
+SELECT tournament_name, tournament_country, tournament_start_date FROM tournaments
+WHERE tournament_id > 2
+AND tournament_country = 'france'
+ORDER BY tournament_country DESC;
 
 SELECT game_id FROM statistics
-WHERE game_duration LIKE "&game_duration"
-AND game_kills LIKE "&game_kills"
-AND game_deaths LIKE "&game_deaths";
+WHERE game_duration LIKE 30
+AND game_kills LIKE 10
+AND game_deaths LIKE 4;
 
 SELECT player_name FROM players
-WHERE player_name LIKE "&player_name%"
+WHERE player_name LIKE 'jo'
 AND contact_email IS NOT NULL;
 
 SELECT prize_amount FROM prizes
-WHERE achievement_name NOT="winner"
+WHERE NOT achievement_name = 'winner'
 AND prize_amount > 100;
 
 SELECT tournament_name, genre_name
@@ -39,6 +31,7 @@ SELECT  tournament_name, genre_name, prize_amount, tournament_start_date
 FROM categories, tournaments, prizes
 WHERE categories.genre_id = tournaments.genre_id
 AND tournaments.tournament_id = prizes.tournament_id
+AND tournament_start_date BETWEEN '01-FEB-2022' AND '01-MAR-2022'
 ORDER BY prize_amount DESC;
 
 SELECT  tournament_name, genre_name, prize_amount, tournament_start_date
